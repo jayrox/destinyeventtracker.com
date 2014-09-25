1d973817589b0f331654bb5262e7a7c20a7ee260
jayrox <jayrox@gmail.com>
2014-09-25 09:38:55 -0400

    Create timers.js
    copied from https://github.com/expressmedia/destinytimers-js
    
    thank you!

---
+++ timers.js
@@ -0,0 +1,343 @@
+[
+    {
+        "name": "Destiny",
+        "events": [
+            {
+                "uniqueId": "reset",
+                "name": "Weekly Reset",
+                "type": 2,
+                "validDays": [
+                    2
+                ],
+                "subEvents": [
+                    "Raid Lockout Reset",
+                    "Vanguard/Crucible Mark Limit Reset"
+                ],
+                "arrivalDay": 2,
+                "arrivalTime": 32400,
+                "departureDay": 2,
+                "departureTime": 32430
+            },
+            {
+                "name": "Bounty Stock Refresh",
+                "type": 3,
+                "subEvents": [
+                    "New Bounties Available"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "resetBounty1",
+                        "start": 32400,
+                        "end": 32430
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "The Tower",
+        "events": [
+            {
+                "uniqueId": "xur",
+                "name": "Xur, Agent of the Nine",
+                "type": 2,
+                "validDays": [
+                    5,
+                    6,
+                    0
+                ],
+                "subEvents": [
+                    "Spawns somewhere randomly in the tower",
+                    "Sells exotic weapons and armor"
+                ],
+                "arrivalDay": 5,
+                "arrivalTime": 32400,
+                "departureDay": 0,
+                "departureTime": 32400
+            }
+        ]
+    },
+    {
+        "name": "Earth",
+        "events": [
+            {
+                "name": "The Steppes",
+                "type": 1,
+                "subEvents": [
+                    "Defeat Extraction Crew"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "steppes1",
+                        "start": 900,
+                        "end": 1500
+                    },
+                    {
+                        "uniqueId": "steppes2",
+                        "start": 2700,
+                        "end": 3300
+                    }
+                ]
+            },
+            {
+                "name": "Mothyards",
+                "type": 1,
+                "subEvents": [
+                    "Defend the Warsat",
+                    "Kill the Target",
+                    "Defeat Extraction Crews"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "moth1",
+                        "start": 0,
+                        "end": 300
+                    },
+                    {
+                        "uniqueId": "moth2",
+                        "start": 1800,
+                        "end": 2100
+                    }
+                ]
+            },
+            {
+                "name": "The Divide",
+                "type": 1,
+                "subEvents": [
+                    "Defeat Devil Walker"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "divide1",
+                        "start": 900,
+                        "end": 1200
+                    },
+                    {
+                        "uniqueId": "divide2",
+                        "start": 2700,
+                        "end": 3000
+                    }
+                ]
+            },
+            {
+                "name": "Rocketyard",
+                "type": 1,
+                "subEvents": [
+                    "Kill Hallowed Knight"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "rocket1",
+                        "start": 900,
+                        "end": 1200
+                    }
+                ]
+            },
+            {
+                "name": "Forgotten Shore",
+                "type": 1,
+                "subEvents": [
+                    "Defeat Extraction Crews"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "shore1",
+                        "start": 300,
+                        "end": 600
+                    },
+                    {
+                        "uniqueId": "shore2",
+                        "start": 2100,
+                        "end": 2400
+                    }
+                ]
+            },
+            {
+                "name": "Skywatch",
+                "type": 1,
+                "subEvents": [
+                    "Defend Warsat",
+                    "Fallen and Hive War"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "skywatch1",
+                        "start": 0,
+                        "end": 300
+                    },
+                    {
+                        "uniqueId": "skywatch2",
+                        "start": 1800,
+                        "end": 2100
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "Venus",
+        "events": [
+            {
+                "name": "Ember Caves",
+                "type": 1,
+                "subEvents": [
+                    "Defeat Extraction Crew"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "ember1",
+                        "start": 1800,
+                        "end": 2100
+                    }
+                ]
+            },
+            {
+                "name": "Ember Caves",
+                "type": 1,
+                "subEvents": [
+                    "Defeat Winter Walker"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "ember2",
+                        "start": 1800,
+                        "end": 2400
+                    }
+                ]
+            },
+            {
+                "name": "Citadel or Ishtar Cliffs",
+                "type": 1,
+                "subEvents": [
+                    "Prevent Vex Sacrifices"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "citadel1",
+                        "start": 2100,
+                        "end": 2400
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "Moon",
+        "events": [
+            {
+                "name": "Archer's Line",
+                "type": 1,
+                "subEvents": [
+                    "Defend the Warsat"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "archer1",
+                        "start": 2100,
+                        "end": 2400
+                    }
+                ]
+            },
+            {
+                "name": "Hellmouth",
+                "type": 1,
+                "subEvents": [
+                    "Defeat Extraction Crews"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "hellmouth1",
+                        "start": 2400,
+                        "end": 2700
+                    }
+                ]
+            },
+            {
+                "name": "Anchor of Light",
+                "type": 1,
+                "subEvents": [
+                    "Defend the Warsat"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "anchorlight1",
+                        "start": 0,
+                        "end": 300
+                    }
+                ]
+            },
+            {
+                "name": "Anchor of Light",
+                "type": 1,
+                "subEvents": [
+                    "Destroy the Iron Walker"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "anchorlight2",
+                        "start": 1800,
+                        "end": 2100
+                    },
+                    {
+                        "uniqueId": "anchorlight3",
+                        "start": 2700,
+                        "end": 3000
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "Mars",
+        "events": [
+            {
+                "name": "The Hollows",
+                "type": 1,
+                "subEvents": [
+                    "Kill Cabal Engineer"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "hollows1",
+                        "start": 1800,
+                        "end": 2100
+                    }
+                ]
+            },
+            {
+                "name": "Buried City",
+                "type": 1,
+                "subEvents": [
+                    "Eliminate Axis Minotaur"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "buried1",
+                        "start": 2700,
+                        "end": 3000
+                    }
+                ]
+            },
+            {
+                "name": "The Barrens",
+                "type": 1,
+                "subEvents": [
+                    "Defend the Warsat",
+                    "Eliminate Axis Minotaur"
+                ],
+                "intervals": [
+                    {
+                        "uniqueId": "barrens1",
+                        "start": 0,
+                        "end": 300
+                    },
+                    {
+                        "uniqueId": "barrens2",
+                        "start": 1800,
+                        "end": 2400
+                    }
+                ]
+            }
+        ]
+    }
+]
